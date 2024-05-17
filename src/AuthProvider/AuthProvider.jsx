d238350f356a356c8bad2747dfb98cd494055e84
Mehedihasan444 <mehedihasan67705251@gmail.com>
2024-05-17 17:46:48 +0600

    fix few bugs

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -23,7 +23,7 @@ const AuthProvider = ({ children }) => {
   // create user with email and password
   const createUserWithEmail = async (email, password) => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      return createUserWithEmailAndPassword(auth, email, password);
     } catch (error) {
       return { errorCode: error.code, errorMessage: error.message };
     }
@@ -32,7 +32,7 @@ const AuthProvider = ({ children }) => {
   // signIn with email and password
   const signInWithEmail = async (email, password) => {
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      return signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
       return { errorCode: error.code, errorMessage: error.message };
     }
@@ -41,7 +41,7 @@ const AuthProvider = ({ children }) => {
   // login with google
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, provider);
+      return signInWithPopup(auth, provider);
     } catch (error) {
       return { errorCode: error.code, errorMessage: error.message };
     }
@@ -50,7 +50,7 @@ const AuthProvider = ({ children }) => {
   // update profile
   const updateProfileInfo = async (name, imgURL) => {
     try {
-      await updateProfile(auth.currentUser, {
+      return updateProfile(auth.currentUser, {
         displayName: name,
         photoURL: imgURL,
       });
@@ -62,8 +62,7 @@ const AuthProvider = ({ children }) => {
   // forget/reset user password
   const resetUserPassword = async (email) => {
     try {
-      await sendPasswordResetEmail(auth, email);
-      return { message: true };
+      return sendPasswordResetEmail(auth, email);
     } catch (error) {
       return { errorCode: error.code, errorMessage: error.message };
     }
@@ -72,8 +71,7 @@ const AuthProvider = ({ children }) => {
   // Email verification
   const sendVerificationEmail = async () => {
     try {
-      await sendEmailVerification(auth.currentUser);
-      return { code: "successful" };
+      return sendEmailVerification(auth.currentUser);
     } catch (error) {
       return { errorCode: error.code, errorMessage: error.message };
     }
@@ -83,7 +81,7 @@ const AuthProvider = ({ children }) => {
   const logOut = async () => {
     setLoading(true);
     try {
-      await signOut(auth);
+      return signOut(auth);
     } catch (error) {
       console.error("Error signing out:", error);
     }
@@ -126,116 +124,3 @@ AuthProvider.propTypes = {
 
 export default AuthProvider;
 
-
-
-// import { createContext, useState } from "react";
-// import auth from "../Firebase/firebase.config";
-// import {
-//   GoogleAuthProvider,
-//   createUserWithEmailAndPassword,
-//   onAuthStateChanged,
-//   sendEmailVerification,
-//   signInWithEmailAndPassword,
-//   signInWithPopup,
-//   signOut,
-//   updateProfile,
-//   sendPasswordResetEmail,
-// } from "firebase/auth";
-// import { useEffect } from "react";
-
-// import PropTypes from "prop-types";
-
-// export const AuthContext = createContext(null);
-// const provider = new GoogleAuthProvider();
-
-// const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-//   // create user with email and password
-
-//   const create_user_with_email = (email, password) => {
-//     return createUserWithEmailAndPassword(auth, email, password);
-//   };
-//   // signIn with email and password
-
-//   const signIn_with_email = (email, password) => {
-//     return signInWithEmailAndPassword(auth, email, password);
-//   };
-
-//   // login with google
-//   const signIn_Google = () => {
-//     return signInWithPopup(auth, provider);
-//   };
-
-//   // update profile
-//   const update_profile = (name, imgURL) => {
-//     return updateProfile(auth.currentUser, {
-//       displayName: name,
-//       photoURL: imgURL,
-//     });
-//   };
-//   // forget/reset user password
-//   const update_password = (email) => {
-//     sendPasswordResetEmail(auth, email)
-//       .then(() => {
-//         return { message: true };
-//       })
-//       .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         return { errorCode, errorMessage };
-//         // ..
-//       });
-//   };
-//   // Email verification
-//   const email_verify = () => {
-//     sendEmailVerification(auth.currentUser).then(() => {
-//       return { code: "successful" };
-//     });
-//   };
-
-//   // sign Out
-//   const logOut = () => {
-//     setLoading(true);
-//     return signOut(auth);
-//   };
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//       if (currentUser) {
-//         console.log(currentUser);
-//         setUser(currentUser);
-//         setLoading(false);
-//       } else {
-//         console.log("no user");
-//         setUser(false);
-//       }
-//     });
-
-//     return () => {
-//       unsubscribe();
-//     };
-//   }, [user]);
-
-//   const info = {
-//     signIn_Google,
-//     update_profile,
-//     create_user_with_email,
-//     signIn_with_email,
-//     logOut,
-//     signInWithPopup,
-//     user,
-//     loading,
-//     update_password,
-//     email_verify,
-//     sendEmailVerification,
-//   };
-
-//   return <AuthContext.Provider value={info}>{children}</AuthContext.Provider>;
-// };
-// export default AuthProvider;
-
-// AuthProvider.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
